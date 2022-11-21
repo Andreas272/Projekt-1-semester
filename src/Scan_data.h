8187a3051e41042f5d5c8b1fb2d7eb5259604173
GusatvHus <gnautr22@student.aau.dk>
2022-11-21 18:31:22 +0100

    Med funktion til at afgøre array_size

--- src/Scan_data.h
+++ src/Scan_data.h
@@ -13,4 +13,5 @@ void data_reader (int time_array[array_size][array_size],int size_of_array);
 int size_of_array();
 void print_array(int time_array[array_size][array_size],int size_of_array);
 
+
 #endif //P1___TRAIN_VS_PLANES_SCAN_DATA_H
