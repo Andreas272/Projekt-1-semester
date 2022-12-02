461aaf035bdacadfe12ee6034a190509435309ec
LeUnioo <mnisse22@student.aau.dk>
2022-12-02 09:40:22 +0100

    plantime_comparison, mark 2 by Mikael

--- src/plane_time_comparison.c
+++ src/plane_time_comparison.c
@@ -2,28 +2,45 @@
 #include "route_cal.h"
 
 
-double plane_cal (int departure, int arrival){
-    char start_airport, end_airport;
-    printf("Choose 2 airports (first start airport, then end airport)\n");
-    printf("Here is the list of airports:\n(f)Frankfurt, (s)Stuttgart, (m)Munich\n");
-    scanf(" %c %c", &start_airport, &end_airport);
+double plane_cal(int departure, int arrival) {
 
-    int speed = 500, extra_time = 4;
     double plane_time = 0;
 
-    if(start_airport == 'f' && end_airport == 'm' || start_airport == 'm' && end_airport == 'f'){
-        int frank_munc = 301;
-        plane_time =  (((1.0/speed)*frank_munc)+extra_time);
-    } else if(start_airport == 'f' && end_airport == 's' || start_airport == 's' && end_airport == 'f') {
-        int frank_stutt = 157;
-        plane_time =  ((1.0/speed)*frank_stutt+extra_time);
-    } else if(start_airport == 'm' && end_airport == 's' || start_airport == 's' && end_airport == 'm') {
-        int munich_stutt = 193;
-        plane_time =  ((1.0/speed)*munich_stutt+extra_time);
-    }
+    // (0)Frankfurt, (1)Stuttgart, (2)Munich
+    if (departure == 1 && arrival == 11 ||
+        departure == 11 && arrival == 1) {
+        int frank_munc = 300;
+        plane_time = 1.5 + (0.002103055473 * frank_munc);
+        return plane_time;
 
+    } else if (departure == 1 && arrival == 6 ||
+               departure == 6 && arrival == 1) {
+        int frank_stutt = 157;
+        plane_time = 1.5 + (0.002103055473 * frank_stutt);
+        return plane_time;
+
+    } else if (departure == 11 && arrival == 6 ||
+               departure == 6 && arrival == 11) {
+        int munich_stutt = 193;
+        plane_time = 1.5 + (0.002103055473 * munich_stutt);
+        return plane_time;
+    }
     return plane_time;
+
+
+
+
 }
+
+
+/*
+printf("Choose 2 airports (first start airport, then end airport)\n");
+printf("Here is the list of airports:\n(f)Frankfurt, (s)Stuttgart, (m)Munich\n");
+scanf(" %c %c", &start_airport, &end_airport);
+ */
+
+
+
 
 void travel_comparison(double plane, int train) {
 
